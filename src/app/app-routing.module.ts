c72b1193108ebd9fa8d0c4a182d5d4b27276c674
CpuJP <cpujuanpis@gmail.com>
2022-11-12 20:47:02 -0500

    configuracion despliegue netlify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'nav-main', component: NavMainComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
